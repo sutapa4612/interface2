66f5fb9cddc8ba18ecde33bdee2f49b090fc5461
sutapa4612 <sutapamukherjee4612@gmail.com>
2018-06-04 23:41:00 -0700

    First commit

---
+++ src/com/It1.java
@@ -0,0 +1,28 @@
+package com;
+
+interface in1
+{
+	void m1();
+	void m2();
+}
+
+public class It1 implements in1
+{
+	public void m1(){
+		System.out.println("m1 method");
+	}
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
+	
+	
+	
+	
+	
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
---
+++ src/com/Testmain.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Testmain {
+	public static void main(String[] args) 
+	{
+	It1 t=new It1();
+	t.m1();
+	t.m2();
+	}
+	
+
+}
